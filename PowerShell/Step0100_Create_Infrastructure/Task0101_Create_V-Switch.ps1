a52e526c4859b6f75dcc6f23cf5a3c6038b4526d
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-09-27 11:15:35 +0200

    Update and rename Task0101_Create_V-Switch to Task0101_Create_V-Switch.ps1

---
+++ PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
@@ -0,0 +1,12 @@
+$SwitchType = Read-Host "Typ [e / i / p]"
+
+IF ($SwitchType -eq "e"){
+    New-VMSwitch -Name "V-Switch-External" -SwitchType External
+}ELSEIF ($SwitchType -eq "i"){
+    New-VMSwitch -Name "V-Switch-Internal" -SwitchType Internal
+}ELSEIF ($SwitchType -eq "p"){
+    New-VMSwitch -Name "V-Switch-Private" -SwitchType Private
+}ELSE {
+    Write-Host "Keine korrekte Eingabe"
+    BREAK
+}
