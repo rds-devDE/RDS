a9fefdbe00f16ce28272e279e4c14e31e2fb8a68
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-10-07 12:45:26 +0200

    Update Task0101_Create_V-Switch.ps1
    Fixed an error for external switch
    Added better interfaces for selection

--- PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
@@ -1,12 +1,19 @@
-$SwitchType = Read-Host "Typ [e / i / p]"
+CLS
+$SwitchType = Read-Host "External [e]`nInternal [i] `nPrivate  [p] `n`nSelect a type"
 
 IF ($SwitchType -eq "e"){
-    New-VMSwitch -Name "V-Switch-External" -SwitchType External
+    CLS
+    (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
+    $aktiverNIC = Read-Host "`nType in one of the above"
+    New-VMSwitch -Name "V-Switch-External" -NetAdapterName $aktiverNIC -AllowManagementOS $true
 }ELSEIF ($SwitchType -eq "i"){
+    CLS
     New-VMSwitch -Name "V-Switch-Internal" -SwitchType Internal
 }ELSEIF ($SwitchType -eq "p"){
-    New-VMSwitch -Name "V-Switch-Private" -SwitchType Private
+    CLS
+    New-VMSwitch -Name "V-Switch-Private" -SwitchType Private 
 }ELSE {
+    CLS
     Write-Host "Keine korrekte Eingabe"
     BREAK
-}
+}   
