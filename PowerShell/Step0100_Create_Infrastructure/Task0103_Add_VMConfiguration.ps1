1092ecb1bdcf1daf481ced486704b81781eb9302
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-10-09 09:53:45 +0200

    Update Task0103_Add_VMConfiguration.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
@@ -3,7 +3,7 @@ Set-TimeZone -Name "W. Europe Standard Time"
 New-Item -Path "C:\Install" -ItemType Directory
 $Summary = ""
 
-#Config
+#Change Device Name
 CLS
 $ClientChange = Read-Host "Change Name [y/n]"
 if ($ClientChange -eq "y") {
@@ -16,18 +16,7 @@ if ($ClientChange -eq "y") {
     $Summary += "`nName:    Name skipped"
 }
 
-CLS
-$DomainChange = Read-Host "Change Domain [y/n]"
-if ($DomainChange -eq "y") {
-    $DomainName = Read-Host "Domain Name"
-    Add-Computer -DomainName $DomainName
-    CLS
-    $Summary += "`nDomain:  $DomainName"
-} else {
-    CLS
-    $Summary += "`nDomain:  Domain skipped"
-}
-
+#Change IPv4 Address
 CLS
 $IPv4Change = Read-Host "Change IPv4 [y/n]"
 if ($IPv4Change -eq "y") {
@@ -45,18 +34,33 @@ if ($IPv4Change -eq "y") {
     $Summary += "`nIPv4:    IPv4 skipped"
 }
 
+#Change IPv4 DNS Server
+cLS
 $IPv4DNSChange = Read-Host "Change DNS [y/n]"
 if ($IPv4DNSChange -eq "y") {
     (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
     $IPv4DNSAdapter = Read-Host "`nType in one of the above"
     $IPv4DNS = Read-Host "DNS [IPv4]"
-    Set-DnsClientServerAddress -InterfaceAlias $IPv4DNSAdapter -ServerAddresses ($IPv4DNS,"8.8.8.8")
+    Set-DnsClientServerAddress -InterfaceAlias $IPv4DNSAdapter -ServerAddresses ($IPv4DNS)
     ipconfig /flushdns
     ipconfig /registerdns
     $Summary += "`nDNS:     $IPv4DNS"
 } else {
     CLS
     $Summary += "`nDNS:     DNS skipped"
+}
+
+#Change Domain
+CLS
+$DomainChange = Read-Host "Change Domain [y/n]"
+if ($DomainChange -eq "y") {
+    $DomainName = Read-Host "Domain Name"
+    Add-Computer -DomainName $DomainName
+    CLS
+    $Summary += "`nDomain:  $DomainName"
+} else {
+    CLS
+    $Summary += "`nDomain:  Domain skipped"
 }
 
 CLS
