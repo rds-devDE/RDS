a3eb45d764a15238b3d9c4e41a05072d73d45c02
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-10-09 09:36:30 +0200

    Update Task0103_Add_VMConfiguration.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
@@ -9,8 +9,10 @@ $ClientChange = Read-Host "Change Name [y/n]"
 if ($ClientChange -eq "y") {
     $ClientName = Read-Host "Client Name"
     Rename-Computer -NewName $ClientName
+    CLS
     $Summary += "`nName:    $ClientName"
 } else {
+    CLS
     $Summary += "`nName:    Name skipped"
 }
 
@@ -19,8 +21,10 @@ $DomainChange = Read-Host "Change Domain [y/n]"
 if ($DomainChange -eq "y") {
     $DomainName = Read-Host "Domain Name"
     Add-Computer -DomainName $DomainName
+    CLS
     $Summary += "`nDomain:  $DomainName"
 } else {
+    CLS
     $Summary += "`nDomain:  Domain skipped"
 }
 
@@ -33,21 +37,34 @@ if ($IPv4Change -eq "y") {
     $IPv4Netmask = Read-Host "Netmask [CIDR]"
     $IPv4Gateway = Read-Host "Gateway [IPv4]"
     New-NetIPAddress -InterfaceAlias $IPv4Adapter -IPAddress $IPv4Address -PrefixLength $IPv4Netmask -DefaultGateway $IPv4Gateway
-    $IPv4DNS = Read-Host "DNS [IPv4]"
-    Set-DnsClientServerAddress -InterfaceAlias $IPv4Adapter -ServerAddresses ($IPv4DNS,"8.8.8.8")
     $Summary += "`nAdapter: $IPv4Adapter 
                  `nAddress: $IPv4Address/$IPv4Netmask 
-                 `nGateway: $IPv4Gateway 
-                 `nDNS:     $IPv4DNS"
+                 `nGateway: $IPv4Gateway"
 } else {
+    CLS
     $Summary += "`nIPv4:    IPv4 skipped"
+}
+
+$IPv4DNSChange = Read-Host "Change DNS [y/n]"
+if ($IPv4DNSChange -eq "y") {
+    (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
+    $IPv4DNSAdapter = Read-Host "`nType in one of the above"
+    $IPv4DNS = Read-Host "DNS [IPv4]"
+    Set-DnsClientServerAddress -InterfaceAlias $IPv4DNSAdapter -ServerAddresses ($IPv4DNS,"8.8.8.8")
+    ipconfig /flushdns
+    ipconfig /registerdns
+    $Summary += "`nDNS:     $IPv4DNS"
+} else {
+    CLS
+    $Summary += "`nDNS:     DNS skipped"
 }
 
 CLS
 Write-Host "Summary: `n$Summary"
 
+
 if ((Read-Host "Restart Now [y/n]") -eq "y"){
     Restart-Computer
 } else {
-    Exit
+    Break
 }
