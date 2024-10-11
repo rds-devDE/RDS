eaa8965e0c6551e3dd4d6d27be493ab616d7cdc6
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-10-11 20:37:53 +0200

    Update Task0103_Add_VMConfiguration.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0103_Add_VMConfiguration.ps1
@@ -1,74 +1,94 @@
-#General
-Set-TimeZone -Name "W. Europe Standard Time"
-New-Item -Path "C:\Install" -ItemType Directory
-$Summary = ""
+$Global:Summary = ""
 
-#Change Device Name
-CLS
-$ClientChange = Read-Host "Change Name [y/n]"
-if ($ClientChange -eq "y") {
-    $ClientName = Read-Host "Client Name"
-    Rename-Computer -NewName $ClientName
-    CLS
-    $Summary += "`nName:    $ClientName"
-} else {
-    CLS
-    $Summary += "`nName:    Name skipped"
+#Functions
+function First {
+    Set-TimeZone -Name "W. Europe Standard Time"
+    New-Item -Path "C:\Install" -ItemType Directory
+    Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+    #Log
+    $Global:Summary += "`nTimeZone: W. Europe Standard Time"
+    $Global:Summary += "`nNewPath:  C:\Install\"
+    $Global:Summary += "`nFirewall: Disabled"
 }
 
-#Change IPv4 Address
-CLS
-$IPv4Change = Read-Host "Change IPv4 [y/n]"
-if ($IPv4Change -eq "y") {
+function Name {
+    $ClientName = Read-Host "Client Name"
+    Rename-Computer -NewName $ClientName
+    $Global:Summary += "`nName:     $ClientName"
+}
+
+function IPv4 {
     (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
     $IPv4Adapter = Read-Host "`nType in one of the above"
     $IPv4Address = Read-Host "Address [IPv4]"
     $IPv4Netmask = Read-Host "Netmask [CIDR]"
     $IPv4Gateway = Read-Host "Gateway [IPv4]"
     New-NetIPAddress -InterfaceAlias $IPv4Adapter -IPAddress $IPv4Address -PrefixLength $IPv4Netmask -DefaultGateway $IPv4Gateway
-    $Summary += "`nAdapter: $IPv4Adapter 
-                 `nAddress: $IPv4Address/$IPv4Netmask 
-                 `nGateway: $IPv4Gateway"
-} else {
-    CLS
-    $Summary += "`nIPv4:    IPv4 skipped"
+    $Global:Summary += "`nAdapter:  $IPv4Adapter 
+                        Address:  $IPv4Address/$IPv4Netmask 
+                        Gateway:  $IPv4Gateway"
 }
 
-#Change IPv4 DNS Server
-cLS
-$IPv4DNSChange = Read-Host "Change DNS [y/n]"
-if ($IPv4DNSChange -eq "y") {
+function DNS {
     (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
     $IPv4DNSAdapter = Read-Host "`nType in one of the above"
-    $IPv4DNS = Read-Host "DNS [IPv4]"
+    $IPv4DNS = Read-Host "`nDNS [IPv4]"
     Set-DnsClientServerAddress -InterfaceAlias $IPv4DNSAdapter -ServerAddresses ($IPv4DNS)
     ipconfig /flushdns
     ipconfig /registerdns
-    $Summary += "`nDNS:     $IPv4DNS"
-} else {
-    CLS
-    $Summary += "`nDNS:     DNS skipped"
+    $Global:Summary += "`nDNS:      $IPv4DNS"
 }
 
-#Change Domain
-CLS
-$DomainChange = Read-Host "Change Domain [y/n]"
-if ($DomainChange -eq "y") {
+function Domain {
     $DomainName = Read-Host "Domain Name"
     Add-Computer -DomainName $DomainName
-    CLS
-    $Summary += "`nDomain:  $DomainName"
-} else {
-    CLS
-    $Summary += "`nDomain:  Domain skipped"
+    $Global:Summary += "`nDomain:   $DomainName"
 }
 
-CLS
-Write-Host "Summary: `n$Summary"
-
-
-if ((Read-Host "Restart Now [y/n]") -eq "y"){
+function Restart {
     Restart-Computer
-} else {
-    Break
+    }
+
+function Summary {
+    Write-Host "Summary: `n$Global:Summary`n"
+    Pause 
+}
+
+#While Loop for each function
+while ($true) {
+    CLS
+    $input = Read-Host "
+    --------------------------
+    Select a function:        
+    --------------------------
+    First installtion: First 
+    Change Name      : Name
+    Change IPv4      : IPv4 
+    Change DNS       : DNS
+    Change Domain    : Domain
+
+    Get Changes Made : Summary
+
+    Restart Computer : Restart                       
+    Exit             : Exit
+    --------------------------
+    "
+    CLS
+    # Abort with Exit
+    if ($input -eq "Exit") {
+        break
+    }
+    
+    # Switch to select the function
+    switch ($input) {
+        "First" { First }
+        "Name" { Name }
+        "IPv4" { IPv4 }
+        "DNS" { DNS }
+        "Domain" { Domain }
+        "Summary" { Summary }
+        "Restart" { Restart }
+        
+        Default { Write-Host "Unknown Input: $input" }
+    }
 }
