96ee2787ad587cebd2a507b2439cc52fa425c3b5
rds-devDE <aaron.schrimpf@rds-dev.de>
2024-11-29 09:30:37 +0100

    Update Task0102_Create_VM.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0102_Create_VM.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0102_Create_VM.ps1
@@ -1,20 +1,19 @@
-# VM Definieren
+# VM variables
 $VmName = Read-Host "Name"
 $RAM = [int](Read-Host "RAM (GB)")*1GB
 $Speicher = [int](Read-Host "Speicher (GB)")*1GB
 $CPU = [int](Read-Host "CPU-Kerne")
 $SwitchType = Read-Host "Switch-Typ [e / i / p]"
 
-# Pfade Erstellen
+# Create paths
 $VMPath = "D:\VM\$VmName"
 $VHDPath = "D:\VM\$VmName\Virtual Hard Disks\$VmName.vhdx"
 
-# VM Erstellen (Generation 2)
+# Create VM with defined variables
 New-VM -Name $VmName -MemoryStartupBytes $RAM -BootDevice VHD -NewVHDPath $VHDPath -NewVHDSizeBytes $Speicher -Path $VMPath -Generation 2
-
-# CPU-Kerne auf 2 setzen
 Set-VMProcessor -VMName $VmName -Count $CPU
 
+# Select Switch
 IF ($SwitchType -eq "e"){
     Connect-VMNetworkAdapter -VMName $VmName -SwitchName "V-Switch-External"
 }ELSEIF ($SwitchType -eq "i"){
