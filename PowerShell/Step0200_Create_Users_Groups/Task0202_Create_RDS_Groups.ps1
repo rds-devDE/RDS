e50eb13081cf19c6f1f498c34e6a4efd24d59c27
rds-devDE <aaron.schrimpf@rds-dev.de>
2025-01-22 09:45:46 +0100

    Update Task0202_Create_RDS_Groups.ps1

--- PowerShell/Step0200_Create_Users_Groups/Task0202_Create_RDS_Groups.ps1
+++ PowerShell/Step0200_Create_Users_Groups/Task0202_Create_RDS_Groups.ps1
@@ -8,6 +8,6 @@ $NameArray = $Names -split ','
 
 Foreach ($Name in $NameArray){
     $Name = $Name.Trim() #Entfernen von versehentlichen Leerzeichen
-    New-ADGroup -Name "B_RDS_$Name" -Path "OU=RDS,OU=Domain Groups,DC=,DC=de" -GroupScope Global -GroupCategory Security -Description "Berechtigung für RDS-Anwendung $Name"
+    New-ADGroup -Name "tg_RDS_$Name" -Path "OU=RDS,OU=Domain Groups,DC=,DC=de" -GroupScope Global -GroupCategory Security -Description "Berechtigung für RDS-Anwendung $Name"
     Write-Host "Sicherheitsgruppe erstellt: B_RDS_$Name"
 }
