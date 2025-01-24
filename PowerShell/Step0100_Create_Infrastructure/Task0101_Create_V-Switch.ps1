2717f89161726967b41aadd122c9763dcc56cdbf
rds-devDE <aaron.schrimpf@rds-dev.de>
2025-01-24 15:28:28 +0100

    Update Task0101_Create_V-Switch.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
@@ -2,18 +2,18 @@
 $SwitchType = Read-Host "External [e]`nInternal [i] `nPrivate  [p] `n`nSelect a type"
 
 IF ($SwitchType -eq "e"){
-    CLS
+    Clear-Host
     (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
     $aktiverNIC = Read-Host "`nType in one of the above"
     New-VMSwitch -Name "V-Switch-External" -NetAdapterName $aktiverNIC -AllowManagementOS $true
 }ELSEIF ($SwitchType -eq "i"){
-    CLS
+    Clear-Host
     New-VMSwitch -Name "V-Switch-Internal" -SwitchType Internal
 }ELSEIF ($SwitchType -eq "p"){
-    CLS
+    Clear-Host
     New-VMSwitch -Name "V-Switch-Private" -SwitchType Private 
 }ELSE {
-    CLS
+    Clear-Host
     Write-Host "Keine korrekte Eingabe"
     BREAK
 }   
