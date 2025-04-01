59b5c584668cf44752e03e8c96d2fe0135e224ea
rds-devDE <aaron.schrimpf@rds-dev.de>
2025-04-01 11:51:34 +0200

    Update Task0101_Create_V-Switch.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0101_Create_V-Switch.ps1
@@ -1,6 +1,7 @@
 # Select Switch Type
 $SwitchType = Read-Host "External [e]`nInternal [i] `nPrivate  [p] `n`nSelect a type"
 
+# Create Switch depending on input
 IF ($SwitchType -eq "e"){
     Clear-Host
     (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
