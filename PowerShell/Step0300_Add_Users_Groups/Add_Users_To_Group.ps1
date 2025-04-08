74111070690b2b69f3c32ab3b2791c3a8749d1fa
rds-devDE <aaron.schrimpf@rds-dev.de>
2025-04-08 10:20:18 +0200

    Update Add_Users_To_Group.ps1

--- PowerShell/Step0300_Add_Users_Groups/Add_Users_To_Group.ps1
+++ PowerShell/Step0300_Add_Users_Groups/Add_Users_To_Group.ps1
@@ -1,22 +1,25 @@
-#Gruppenauswahl
-$GroupSearch = Read-Host "Search groups"
+# Import Modules
+Import-Module ActiveDirectory
 
+# User Input for Group Name
+$GroupSearch = Read-Host "Search groups"
 $GroupLookup = Get-ADGroup -Filter "Name -like '*$GroupSearch*'"
 $GroupLookup | Format-Table Name
 
+# User Input for exact Group Name
 $GroupExact = Read-Host "Add to this specific group"
 $GroupAD = Get-ADGroup -Filter "Name -eq '$GroupExact'"
 
-#Userauswahl
+# User Input for exact User
 $UserSearch = Read-Host "Search Users"
-
 $UserLookup = Get-ADUser -Filter "Name -like '*$UserSearch*'"
 $UserLookup | Format-Table Name
 
+# User Input for exact User Name
 $UserExact = Read-Host "Select all users (separate through commas)"
-
 $UserArray = $UserExact -split ','
 
+# Loop for each User in UserArray
 Foreach ($User in $UserArray) {
     $User = $User.Trim()
     Add-ADGroupMember -Members $User -Identity $GroupAD
