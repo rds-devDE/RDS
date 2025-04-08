2e88cd3afd2fb7ff202e4be22610f796442a9bc3
rds-devDE <aaron.schrimpf@rds-dev.de>
2025-04-08 10:30:45 +0200

    Update Task0103_VMConfig.ps1

--- PowerShell/Step0100_Create_Infrastructure/Task0103_VMConfig.ps1
+++ PowerShell/Step0100_Create_Infrastructure/Task0103_VMConfig.ps1
@@ -1,18 +1,18 @@
-# Rename Computer
+# User Input for Computer
 $ClientName = Read-Host "Client Name"
 Rename-Computer -NewName $ClientName
 
-# Select Domain
+# User Input for Domain
 $DomainName = Read-Host "Domain Name"
 Add-Computer -DomainName $DomainName
 
-# Select TimeZone
+# Set TimeZone
 Set-TimeZone -Name "W. Europe Standard Time"
 
-# Create Standard Paths
+# Set Standard Path
 New-Item -Path "C:\Install" -ItemType Directory
 
-# Give a static IP with Gateway and DNS
+# Set static IP with Gateway and DNS
 (Get-NetAdapter | Where-Object {$_.Status -eq 'Up'}).Name
 $IPv4Adapter = Read-Host "`nType in one of the above"
 $IPv4Address = Read-Host "Address [IPv4]"
@@ -20,7 +20,7 @@ $IPv4Netmask = Read-Host "Netmask [CIDR]"
 $IPv4Gateway = Read-Host "Gateway [IPv4]"
 $IPv4DNS = Read-Host "DNS     [IPv4]"
 New-NetIPAddress -InterfaceAlias $IPv4Adapter -IPAddress $IPv4Address -PrefixLength $IPv4Netmask -DefaultGateway $IPv4Gateway
-Set-DnsClientServerAddress -InterfaceAlias $IPv4DNSAdapter -ServerAddresses ($IPv4DNS)
+Set-DnsClientServerAddress -InterfaceAlias $IPv4Adapter -ServerAddresses ($IPv4DNS)
 ipconfig /flushdns
 ipconfig /registerdns
 
